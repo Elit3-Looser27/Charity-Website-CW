16d9a74374c94244e4c4dd8988b16c91809876ab
Begad Hatem Diab <114566387+Elit3-Looser27@users.noreply.github.com>
2023-12-01 21:12:15 +0200

    updated database

--- sql/Charity_db.sql
+++ sql/Charity_db.sql
@@ -1,115 +1,28 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 01 ديسمبر 2023 الساعة 19:50
--- إصدار الخادم: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+CREATE TABLE donation_info (
+  id int NOT NULL IDENTITY(1,1),
+  name varchar(128) NOT NULL,
+  email varchar(255) NOT NULL,
+  gender varchar(10) NOT NULL, -- Changed from ENUM
+  address varchar(255) NOT NULL,
+  phone_number varchar(15) NOT NULL,
+  charity varchar(255) NOT NULL,
+  donation_type varchar(255) NOT NULL,
+  specify_type varchar(255) NOT NULL,
+  amount decimal(10,2) NOT NULL,
+  payment_method varchar(255) NOT NULL,
+  card_number varchar(16) NOT NULL,
+  card_expiry date NOT NULL,
+  cvv int NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (card_number)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `resit_db`
---
-
--- --------------------------------------------------------
-
---
--- بنية الجدول `donation_info`
---
-
-CREATE TABLE `donation_info` (
-  `id` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `gender` enum('male','female','other') NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `phone_number` varchar(15) NOT NULL,
-  `charity` varchar(255) NOT NULL,
-  `donation_type` varchar(255) NOT NULL,
-  `specify_type` varchar(255) NOT NULL,
-  `amount` decimal(10,2) NOT NULL,
-  `payment_method` varchar(255) NOT NULL,
-  `card_number` varchar(16) NOT NULL,
-  `card_expiry` date NOT NULL,
-  `cvv` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- إرجاع أو استيراد بيانات الجدول `donation_info`
---
-
-INSERT INTO `donation_info` (`id`, `name`, `email`, `gender`, `address`, `phone_number`, `charity`, `donation_type`, `specify_type`, `amount`, `payment_method`, `card_number`, `card_expiry`, `cvv`) VALUES
-(47, 'Begad Hatem Diyab', 'hbegadhatem17@gmail.com', 'male', 'Taj City Compound', '+201026088401', 'Egyptian Food Bank', 'funds', '', '2.00', 'cash', 'CASH-64aaea293d9', '0000-00-00', 0),
-(48, 'Begad Hatem Diyab', 'hbegadhatem17@gmail.com', 'male', 'Taj City Compound', '+201026088401', 'Egyptian Food Bank', 'funds', '', '2.00', 'cash', 'CASH-64aaeaaf5b8', '0000-00-00', 0),
-(49, 'Begad Hatem Diyab', 'hbegadhatem17@gmail.com', 'male', 'Taj City Compound', '01026088401', 'Children Cancer Hospital 57357', 'other-item', 'clothes', '200.00', 'online', '1234567890', '0000-00-00', 123),
-(50, 'Begad Hatem Diyab', 'hbegadhatem17@gmail.com', 'male', 'Taj City Compound', '01026088401', 'Ahl Masr Hospital', 'funds', '', '200.00', 'online', '0987761637123', '2027-11-05', 456),
-(51, 'ahmed', 'ahmed@gmail.com', 'male', 'cairo', '12345678900', 'Resala Organization', 'other-item', 'clothes', '123.00', 'online', '0099128132940132', '2028-10-09', 123456);
-
--- --------------------------------------------------------
-
---
--- بنية الجدول `feedback`
---
-
-CREATE TABLE `feedback` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `message` text DEFAULT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- إرجاع أو استيراد بيانات الجدول `feedback`
---
-
-INSERT INTO `feedback` (`id`, `name`, `email`, `message`, `timestamp`) VALUES
-(5, 'Begad', 'Begad@gmail.com', 'I Loved The service', '2023-10-19 13:21:19');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `donation_info`
---
-ALTER TABLE `donation_info`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `card_number` (`card_number`);
-
---
--- Indexes for table `feedback`
---
-ALTER TABLE `feedback`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `donation_info`
---
-ALTER TABLE `donation_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
-
---
--- AUTO_INCREMENT for table `feedback`
---
-ALTER TABLE `feedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Note: You may need to adjust ENUM 'gender' to a varchar and manage allowed values through application logic or a lookup table.
+CREATE TABLE feedback (
+  id int NOT NULL IDENTITY(1,1),
+  name varchar(255) NULL,
+  email varchar(255) NULL,
+  message varchar(MAX) NULL, -- Changed from TEXT
+  timestamp datetime NOT NULL DEFAULT GETDATE(), -- Changed from TIMESTAMP
+  PRIMARY KEY (id)
+);
--- sql/Charity_user.sql
+++ sql/Charity_user.sql
@@ -1,75 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 01 ديسمبر 2023 الساعة 19:53
--- إصدار الخادم: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+CREATE TABLE [user] (
+  id int NOT NULL IDENTITY(1,1),
+  name varchar(128) NOT NULL,
+  email varchar(255) NOT NULL,
+  password_hash varchar(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (email)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `resit_user`
---
-
--- --------------------------------------------------------
-
---
--- بنية الجدول `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `name` varchar(128) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password_hash` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- إرجاع أو استيراد بيانات الجدول `user`
---
-
-INSERT INTO `user` (`id`, `name`, `email`, `password_hash`) VALUES
-(1, 'Begad', 'Begad@gmail.com', '$2y$10$T8E7tnXNDDr1gw/nVUEvOeaWd0v97Qq.kQJzYWboYg9bGdS/MH0s2'),
-(11, 'Amal', 'Amal@gmail.com', '$2y$10$1UTwRKQ3xnD9qiFZuVCrauqbIP0dZvKnklV9LJm/V2JorejrTaQ/e'),
-(12, 'Ali', 'ali@gmail.com', '$2y$10$Um6bSztLiqldbqBf7G5YhuErxadLaBuHvKQAjwZKMWv.OMaqsjRu6'),
-(13, 'ahmed', 'ahmed@gmail.com', '$2y$10$ysKyoYt6hC2gzBdzBIIHWumr0BbJevcFuuhYEX6Hw7Zd8HJ9hvyzK'),
-(14, 'Anass', 'Admin-1@gmail.com', '$2y$10$g.DUBC33/8b6XeKaHoHtYelhvwA4plHy74L2NxtCa2dDZz3UlVM3C'),
-(16, 'Kenzy', 'Admin-2@gmail.com', '$2y$10$UHko0b5diBpUmCmOytcf6ub1X6i5YsFWYlETeXt0n4o.2XIB28Y5K'),
-(17, 'Fady', 'Admin-3@gmail.com', '$2y$10$aaPkSowmrJ9xpnN53oMwn.Mp/t2jpALEXI.toA4UWBwVuZ7JHs3cO'),
-(18, 'Jana', 'Jana-Emp-1@gmail.com', '$2y$10$2Vu7SwASq7MK0GAyNd98quQML4ZE1jafCKV3mwkR.qHRwnMYSIxHm');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Note: The data types used in MySQL are generally compatible with SQL Server. 
+-- The main change here is converting AUTO_INCREMENT to IDENTITY.
